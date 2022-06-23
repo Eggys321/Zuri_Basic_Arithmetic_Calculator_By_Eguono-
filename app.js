8b904f5623d01db44b6760d2cb1e7f924a571ae1
Eggys321 <imonieroheguono@gmail.com>
2022-06-24 00:28:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let add = (num1 , num2)=>{
+   let addedNumbers = num1 + num2;
+   console.log(addedNumbers);
+
+}
+add(2,4)
+let subtraction = (num1 , num2)=>{
+   let subtractedNumbers = num1 - num2;
+   console.log(subtractedNumbers)
+
+}
+subtraction(6, 4)
+let multiplication = (num1 , num2)=>{
+   let multipliedNumbers = num1 * num2;
+   console.log(multipliedNumbers)
+
+}
+multiplication(2, 2)
+let Division = (num1 , num2)=>{
+   let dividedNumbers = num1 / num2;
+   console.log(dividedNumbers)
+
+}
+Division(10, 2)
+
